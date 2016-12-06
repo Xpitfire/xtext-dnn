b60f6b528abba0f5374f44be720f71ce61cdb0e4
Marius-Constantin Dinu <dinu.marius-constantin@hotmail.com>
2016-12-06 14:06:00 +0100

    Added VS code support package (not tested)

---
+++ vscode-extension-self-contained/src/extension.ts
@@ -0,0 +1,34 @@
+'use strict';
+
+import * as path from 'path';
+
+import {Trace} from 'vscode-jsonrpc';
+import { workspace, ExtensionContext } from 'vscode';
+import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient';
+
+export function activate(context: ExtensionContext) {
+    // The server is a locally installed in src/dls
+    let script = context.asAbsolutePath(path.join('src', 'dls', 'bin', 'dls-standalone'));
+    
+    let serverOptions: ServerOptions = {
+        run : { command: script },
+        debug: { command: script, args: ['-Xdebug','-Xrunjdwp:server=y,transport=dt_socket,address=8000,suspend=n,quiet=y','-Xmx256m'] }
+    };
+    
+    let clientOptions: LanguageClientOptions = {
+        documentSelector: ['dls'],
+        synchronize: {
+            fileEvents: workspace.createFileSystemWatcher('**/*.*')
+        }
+    };
+    
+    // Create the language client and start the client.
+    let lc = new LanguageClient('Xtext Server', serverOptions, clientOptions);
+    // enable tracing (.Off, .Messages, Verbose)
+    lc.trace = Trace.Verbose;
+    let disposable = lc.start();
+    
+    // Push the disposable to the context's subscriptions so that the 
+    // client can be deactivated on extension deactivation
+    context.subscriptions.push(disposable);
+}
